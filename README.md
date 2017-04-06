9d7252eb7deffe892638dcaf54760504f938f140
morgan <uoozhang@gmail.com>
2017-04-06 11:37:14 -0500

    base64 encoding support; README update

--- README.md
+++ README.md
@@ -2,5 +2,17 @@
 Google Spreadsheets and Python 
 https://www.twilio.com/blog/2017/02/an-easy-way-to-read-and-write-to-a-google-spreadsheet-in-python.html
 
+gspread API Reference http://gspread.readthedocs.io/en/latest/
+
+#Format
+Encoding|link\_or\_cmd|cmd\_or\_comment|comment(optional)
+
 #Dependencies
 pip install --user -r requirements.txt
+
+#Sample usage:
+`gspreadpython.py -e ascii -i "sudo apt-get install libffi-dev" "http://stackoverflow.com/questions/12982486/glib-compile-error-ffi-h-but-libffi-is-installed/17518165#17518165" "fatal error: ffi.h: No such file or directory"`
+
+`gspreadpython.py -e ascii -s ffi.h`
+
+To get one line base64: `cat gspreadpython.py | base64 | tr -d '\n'`
--- gspreadpython.py
+++ gspreadpython.py
@@ -7,14 +7,47 @@ from oauth2client.service_account import ServiceAccountCredentials
 INSERT = 'insert'
 SEARCH = 'search'
 
+ENCODING = 'encoding'
+ASCII = "ascii"
+BASE64 = "base64"
+
 def parse_args():
     parser = argparse.ArgumentParser(prog='gspreadpython.py')
     parser.add_argument('-i','--insert', nargs='+', metavar="item")
     parser.add_argument('-s','--search', nargs='+', metavar="keyword")
+    parser.add_argument('-e','--encoding', metavar="encoding [ascii|base64] ", choices=[ASCII,BASE64], required=True)
     args = vars(parser.parse_args())
     if not any(args.values()):
         parser.error('No arguments provided.')
     return args
+
+def rank_rows(rows, keywords):
+    rows_with_cnt = []
+
+    def get_keywords_cnt(row):
+        cnt = 0
+        for kw in keywords:
+            if kw.lower() in "".join(row).lower(): cnt += 1
+        return cnt
+
+    #get rows with keywords cnt > 0, sort in dsc order, remove num of keywords in the tuple
+    for row in rows:
+        rows_with_cnt.append((row, get_keywords_cnt(row)))
+    rows_with_kw_cnt = filter(lambda x:x[1]>0, rows_with_cnt)
+    rows_with_kw =map(lambda x:x[0],sorted(rows_with_kw_cnt, reverse=True, key=lambda x:x[1]))
+    return rows_with_kw
+
+def is_base64_encoded(s):
+    '''
+    Not a garrantee even if no exception is raised
+    http://stackoverflow.com/a/12315449
+    '''
+    import binascii
+    try:
+        s.decode("base64")
+        return True
+    except binascii.Error:
+        return False
 
 def main():
     args = parse_args()
@@ -26,13 +59,33 @@ def main():
     sheet = client.open("gspreaddb").sheet1
 
     if args[INSERT]:
-        sheet.insert_row(args[INSERT])
+        #inserting encoding|link_or_cmd|cmd_or_comment|comments(optional)
+        if args[ENCODING] == BASE64:
+            if is_base64_encoded(args[INSERT][0]):#verify that the fisrt element in -i list should follow the ENCODING
+                sheet.insert_row([args[ENCODING]] + args[INSERT]) 
+            else:
+                print "string \033[1m%s\033[0m doesnot match the encoding \033[1m%s\033[0m" % (args[INSERT][0], args[ENCODING])
+
+        else:#ascii encoding
+            sheet.insert_row([args[ENCODING]] + args[INSERT]) 
+
     if args[SEARCH]:
         keywords = args[SEARCH]
-        rows = sheet.get_all_values()
-        for row in rows:
-            if filter(lambda x:x in "".join(row), keywords):
-                print "\n\t".join(row)
+        rows = sheet.get_all_values()#might become a problem when the sheet gets larger
+
+        rows = filter(lambda row:row[0] == args[ENCODING], rows)
+
+        #decode base64 on the 2nd column (1st column is encoding)
+        if args[ENCODING] == BASE64:
+            for row in rows:
+                row[1] = row[1].decode("base64")
+
+        rows_ranked = rank_rows(rows, keywords)
+        for i, row in enumerate(rows_ranked): 
+            if i == 0: print "\033[1m"
+            row_without_encoding = row[1:]
+            print "\n\t".join(row_without_encoding)
+            if i == 0: print "\033[0m"
 
 if __name__ == "__main__":
     main() 
